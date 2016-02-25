3242588155213bc99ea60801c197fad8692a7fae
Sonoflight <coreygumbs@gmail.com>
2016-02-25 16:23:34 -0500

    Test completed and passed

--- test/__api.js
+++ test/__api.js
@@ -1,7 +1,22 @@
-var server = require('../src/server.js');
-var request = require('supertest');
-
+var request =  require('supertest');
 
 describe('API', function(){
+	var server;
 
+	beforeEach(function(){
+		server = require('../src/server.js'); 
+	});
+
+	afterEach(function(){
+		server.close();
+	});
+
+	it('/v1/ should return specified object', function apiTest(done){
+		request(server)
+			.get('/api/v1')
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200, {hello: "world"}, done)
+
+	});
 });
