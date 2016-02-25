50d6886511cae31224b77dbbb2a3b7ca67f5b78b
Sonoflight <coreygumbs@gmail.com>
2016-02-25 15:32:53 -0500

    Created routes directory and api.js file

--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ var app = express();
 //Config port for server
 var port = 3000; 
 
+//routes
+app.get('/', function(req, res){
+	res.json({hello: "world"});
+});
+
+
 app.listen(port, function(){
 	console.log("Server Active on ", port);
 });    
